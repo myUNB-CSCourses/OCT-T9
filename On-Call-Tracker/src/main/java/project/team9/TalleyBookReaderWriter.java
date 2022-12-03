7cdd84c63962efa9a66f2905d5b73f6b790a81b4
Marno Labuschagne <mlabusch@unb.ca>
2022-12-03 19:25:07 -0400

    Update TalleyBookReaderWriter.java
    Created a reader that receives a month and day variable. Prints out the tallies for that day

--- On-Call-Tracker/src/main/java/project/team9/TalleyBookReaderWriter.java
+++ On-Call-Tracker/src/main/java/project/team9/TalleyBookReaderWriter.java
@@ -10,6 +10,100 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class TalleyBookReaderWriter {
 
+	public void readTalleyCountDay(int day, String month){
+		boolean configuration = true;
+		Row row;
+		Cell cell;
+		Iterator<Cell> cellIterator;
+		int period = 0;
+		int dateIndex = 0;
+		int monthlyIndex = 0;
+		int totalIndex = 0;
+		int remainingIndex = 0;
+		int priorityIndex = 0;
+		
+		try {
+			File file = new File("C:\\Users\\marno\\OneDrive\\Uni\\Year 2\\Sem 1\\CS2043\\repo\\OCT-T9\\On-Call-Tracker\\src\\inputs\\On-call_Tallies.xlsx");
+			FileInputStream fis = new FileInputStream(file);
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+			XSSFSheet sheet = wb.getSheet(month);
+			Iterator<Row> itr = sheet.iterator();
+			
+			while(itr.hasNext()) {
+				row = itr.next();
+				cellIterator = row.cellIterator();
+				
+				if (configuration) {
+					outerloop:
+					while(cellIterator.hasNext()) {
+						cell = cellIterator.next();
+						switch (cell.getCellType()) {
+						case STRING:
+							switch (cell.getStringCellValue()){
+							case "Monthly":
+								monthlyIndex = cell.getColumnIndex();
+								if (dateIndex != 0) {
+									configuration = false;
+								}
+								break;
+							case "Total":
+								totalIndex = cell.getColumnIndex();
+								break;
+							case "Remaining":
+								remainingIndex = cell.getColumnIndex();
+								break;
+							case "High Priority":
+								priorityIndex = cell.getColumnIndex();
+								break;
+							default:
+								break;
+							}
+							break;
+						case NUMERIC:
+							if (cell.getNumericCellValue() == day) {
+								dateIndex = cell.getColumnIndex();
+								break outerloop;
+							}
+							break;
+						default:
+							break;
+						}
+					}
+				} else {
+					String title = row.getCell(1).getStringCellValue();
+					if (title.equals("Period 1") || title.equals("Period 2") || title.equals("Period 3") || title.equals("Period 4")) {
+						if (period == 4) {
+							System.out.println("\nTOTALS:");
+							row = itr.next();
+							System.out.println("Period 1" + row.getCell(2).getNumericCellValue());
+							row = itr.next();
+							System.out.println("Period 2" + row.getCell(2).getNumericCellValue());
+							row = itr.next();
+							System.out.println("Period 3" + row.getCell(2).getNumericCellValue());
+							row = itr.next();
+							System.out.println("Period 4" + row.getCell(2).getNumericCellValue());
+							break;
+						}
+						period++;
+						System.out.println("------\nPERIOD " + period + "\n------\n");
+					} else if (title != null) {
+						System.out.println("teacher: " + title + ":");
+						System.out.println("period " + period + ": " + (int)row.getCell(dateIndex).getNumericCellValue());
+						System.out.println("Monthly Total: " + (int)row.getCell(monthlyIndex).getNumericCellValue());
+						System.out.println("Total On Calls: " + (int)row.getCell(totalIndex).getNumericCellValue());
+						System.out.println("Remaining: " + (int)row.getCell(remainingIndex).getNumericCellValue());
+						System.out.println("Priority: " + (int)row.getCell(priorityIndex).getNumericCellValue() + "\n");
+					}
+				}
+			}
+			wb.close();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	/*
+	@SuppressWarnings("unlikely-arg-type")
 	public ArrayList<Teacher> readTalleyCount(String path) {
 		
 		boolean configuration = true;
@@ -92,5 +186,5 @@ public class TalleyBookReaderWriter {
 		
 	}
 	
-	public void writeTalleyCount(String path, String fileName) {}
+	public void writeTalleyCount(String path, String fileName) {}*/
 }
