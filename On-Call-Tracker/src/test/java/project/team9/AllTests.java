3108dae3c5a5fb82f6f8e69c16d5c1ffb82464a1
dmudugam <dineth.m@unb.ca>
2022-12-06 02:20:40 -0400

    Created AllTests.java
    Created AllTests.java to run all the Junit test cases.

---
+++ On-Call-Tracker/src/test/java/project/team9/AllTests.java
@@ -0,0 +1,11 @@
+package project.team9;
+
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SelectClasses;
+
+
+@Suite
+@SelectClasses ({TeacherTest.class, CourseTest.class, MasterScheduleReaderTest.class, ScheduleTest.class})
+class AllTests {
+
+}
