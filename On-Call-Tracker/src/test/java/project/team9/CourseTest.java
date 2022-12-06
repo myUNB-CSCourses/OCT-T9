03c43b247dc8b1af878b8ae6b45464da6aa8874b
dmudugam <dineth.m@unb.ca>
2022-12-05 22:20:53 -0400

    Creating CourseTest.java for Unit Testing
    JUNIT test case java file for Course.java

---
+++ On-Call-Tracker/src/test/java/project/team9/CourseTest.java
@@ -0,0 +1,14 @@
+package project.team9;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CourseTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
